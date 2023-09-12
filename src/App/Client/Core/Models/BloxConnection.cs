07a0726d91fa6d3e6750255fcd71500f1a4303e7
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-09-12 21:40:40 +0330

    Add BloxConnection

--- src/App/Client/Core/Models/BloxConnection.cs
+++ src/App/Client/Core/Models/BloxConnection.cs
@@ -9,11 +9,18 @@ namespace Functionland.FxBlox.Client.Core.Models
     /// <summary>
     /// This is the item that UI binds to
     /// </summary>
-    public class BloxInfo
+    public class BloxConnection : IDisposable
+
     {
-        public required BloxDevice Device { get; set; }
-        public List<BloxStack> Stacks { get; set; } = new();
-        public required IBloxWifiClient WifiClient { get; set; }
-        public required IBloxLibp2pClient Libp2pClient { get; set; }
+    public required BloxDevice Device { get; set; }
+    public List<BloxStack> Stacks { get; set; } = new();
+    public required IBloxWifiClient WifiClient { get; set; }
+    public required IBloxLibp2pClient Libp2pClient { get; set; }
+
+    public void Dispose()
+    {
+        WifiClient.Dispose();
+        Libp2pClient.Dispose();
+    }
     }
 }
