474df39d06eaec09f3a649e8d1eb0949137d18b6
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-09-13 01:26:17 +0330

    Add FakeBloxLibp2pClient for testing purposes

--- src/App/Client/Core/Services/Contracts/IBloxLibp2pClient.cs
+++ src/App/Client/Core/Services/Contracts/IBloxLibp2pClient.cs
@@ -1,6 +1,11 @@
-﻿namespace Functionland.FxBlox.Client.Core.Services.Contracts;
+﻿using Functionland.FxBlox.Client.Core.Models;
+
+namespace Functionland.FxBlox.Client.Core.Services.Contracts;
 
 public interface IBloxLibp2pClient : IDisposable
 {
+    BloxDevice Device { get; }
+    Task InitializeAsync(CancellationToken cancellationToken);
+    Task<bool> CheckConnectionAsync(CancellationToken cancellationToken);
 
 }
---
+++ src/App/Client/Core/Services/Implementations/FakeBloxLibp2pClient.cs
@@ -0,0 +1,29 @@
+﻿using Functionland.FxBlox.Client.Core.Models;
+
+namespace Functionland.FxBlox.Client.Core.Services.Implementations;
+
+public class FakeBloxLibp2pClient : IBloxLibp2pClient
+{
+    public BloxDevice Device { get; }
+
+    public FakeBloxLibp2pClient(BloxDevice device)
+    {
+        Device = device;
+    }
+
+    public async Task InitializeAsync(CancellationToken cancellationToken)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+    }
+
+    public async Task<bool> CheckConnectionAsync(CancellationToken cancellationToken)
+    {
+        await Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+        return true;
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
