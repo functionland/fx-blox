f969046329d86f9eec2c1883313664fdbb81c595
Afshin Alizadeh <afshin.alizadeh.cs@live.com>
2023-09-13 23:43:04 +0330

    Add MyDevice page.

---
+++ src/App/Client/Core/Pages/MyDevicePage/MyDevice.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace Functionland.FxBlox.Client.Core.Pages
+{
+    public partial class MyDevice
+    {
+        private bool _applyAnimation = false;
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                _applyAnimation = true;
+                StateHasChanged();
+            }
+        }
+    }
+}
