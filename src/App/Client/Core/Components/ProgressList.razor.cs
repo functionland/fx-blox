d16e2aceb0608ad9dd7e88778dc26c8b37aa6f5b
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-09-22 15:56:43 +0330

    Refactor and make ProgressList

---
+++ src/App/Client/Core/Components/ProgressList.razor.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Functionland.FxBlox.Client.Core.Models;
+
+namespace Functionland.FxBlox.Client.Core.Components;
+
+public partial class ProgressList
+{
+    [Parameter]
+    public List<ProgressItem> ProgressItems { get; set; } = new List<ProgressItem>();
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+    }
+    
+}
