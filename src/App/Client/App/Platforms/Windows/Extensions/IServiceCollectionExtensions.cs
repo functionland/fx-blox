90f582c60a31be0d5d57b2816cf14a1615c2f35b
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-11-10 23:16:53 +0330

    Register FakeWifiService for windows

--- src/App/Client/App/Platforms/Windows/Extensions/IServiceCollectionExtensions.cs
+++ src/App/Client/App/Platforms/Windows/Extensions/IServiceCollectionExtensions.cs
@@ -14,6 +14,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddSingleton<IPlatformTestService, WindowsPlatformTestService>();
             services.AddTransient<WindowsWalletServiceTransferSomeMoneyPlatformTest>();
             services.AddTransient<WindowsWalletServiceConnectToWalletPlatformTest>();
+            services.AddSingleton<IWifiService, FakeWifiService>();
             return services;
         }
     }
